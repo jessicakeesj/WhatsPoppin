9d19d6180c07286214501e39986ee27027b76a64
jessicakeesj <jksj98@gmail.com>
2019-09-04 16:17:41 +0800

    Commit SignIn

--- app/src/main/java/com/example/whatspoppin/Splashscreen.java
+++ app/src/main/java/com/example/whatspoppin/Splashscreen.java
@@ -17,18 +17,18 @@ public class Splashscreen extends AppCompatActivity {
         Thread thread=new Thread(){
             @Override
             public void run() {
-                for (progress=30;progress<300;progress=progress+10){
                     try {
-                        sleep(3000); //run for 3 secs then sleep
-                        progressBar.setProgress(progress);
+                        for (progress=10;progress<100;progress=progress+10) {
+                            sleep(300); //run for 3 secs then sleep
+                            progressBar.setProgress(progress);
+                        }
                         //sleep(3000); //run for 3 secs then sleep
-                        Intent intent= new Intent(getApplicationContext(),MainActivity.class); //to direct it to this activity after the splash screen finishes
+                        Intent intent= new Intent(getApplicationContext(),SignIn.class); //to direct it to this activity after the splash screen finishes
                         startActivity(intent);
                         finish(); //to stop it from rerunning
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-                }
             }
         };
         thread.start();//to start the method
