62681ac0ab5dff7ac17da1ec6cd067ee816a1997
Eunice <eunicepoh98@gmail.com>
2019-09-12 21:19:15 +0800

    Preference and Fab all icon

--- app/src/main/java/com/example/whatspoppin/ui/editpreferences/EditPreferencesFragment.java
+++ app/src/main/java/com/example/whatspoppin/ui/editpreferences/EditPreferencesFragment.java
@@ -8,11 +8,18 @@ import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.whatspoppin.R;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipDrawable;
+import com.google.android.material.chip.ChipGroup;
+
+import java.util.ArrayList;
 
 public class EditPreferencesFragment extends Fragment {
 
@@ -23,13 +30,30 @@ public class EditPreferencesFragment extends Fragment {
         editPreferencesViewModel =
                 ViewModelProviders.of(this).get(EditPreferencesViewModel.class);
         View root = inflater.inflate(R.layout.fragment_editpreferences, container, false);
-        final TextView textView = root.findViewById(R.id.text_editpreferences);
-        editPreferencesViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+//        final TextView textView = root.findViewById(R.id.text_editpreferences);
+//        editPreferencesViewModel.getText().observe(this, new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
+
+        // Populate Preferences Category
+        ChipGroup preferenceCG = root.findViewById(R.id.preference_chipgroup);
+        String[] categories = {"Attractions", "Conference", "Convention", "Expo", "Festival", "Gala", "Game",
+                "Network", "Party", "Performance", "Race", "Rally", "Retreat", "Screening", "Seminar", "Tour", "Tournament"};
+
+        for (int i = 0; i < categories.length; i++) {
+            Chip newChip = new Chip(getContext());
+            newChip.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));
+            newChip.setChipDrawable(ChipDrawable.createFromAttributes(getContext(), null,
+                    0, R.style.Widget_MaterialComponents_Chip_Filter));
+            newChip.setText(categories[i]);
+            preferenceCG.addView(newChip);
+
+        }
+
         return root;
     }
 }
