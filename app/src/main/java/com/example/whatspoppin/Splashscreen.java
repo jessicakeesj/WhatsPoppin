526e504d2f82ccde9795d063ba22842e512abef2
Eunice <eunicepoh98@gmail.com>
2019-09-27 21:12:47 +0800

    Skip Login

--- app/src/main/java/com/example/whatspoppin/Splashscreen.java
+++ app/src/main/java/com/example/whatspoppin/Splashscreen.java
@@ -1,6 +1,7 @@
 package com.example.whatspoppin;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.widget.ProgressBar;
 
 import android.content.Intent;
@@ -9,26 +10,28 @@ import android.os.Bundle;
 public class Splashscreen extends AppCompatActivity {
     private ProgressBar progressBar;
     int progress;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
         progressBar = findViewById(R.id.pBar);
-        Thread thread=new Thread(){
+        Thread thread = new Thread() {
             @Override
             public void run() {
-                    try {
-                        for (progress=10;progress<100;progress=progress+10) {
-                            sleep(300); //run for 3 secs then sleep
-                            progressBar.setProgress(progress);
-                        }
-                        //sleep(3000); //run for 3 secs then sleep
-                        Intent intent= new Intent(getApplicationContext(),SignIn.class); //to direct it to this activity after the splash screen finishes
-                        startActivity(intent);
-                        finish(); //to stop it from rerunning
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
+                try {
+                    for (progress = 10; progress < 100; progress = progress + 10) {
+                        sleep(300); //run for 3 secs then sleep
+                        progressBar.setProgress(progress);
                     }
+                    //sleep(3000); //run for 3 secs then sleep
+//                        Intent intent= new Intent(getApplicationContext(),SignIn.class); //to direct it to this activity after the splash screen finishes
+                    Intent intent = new Intent(getApplicationContext(), NavDrawer.class); //to direct it to this activity after the splash screen finishes
+                    startActivity(intent);
+                    finish(); //to stop it from rerunning
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         };
         thread.start();//to start the method
