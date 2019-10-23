e9b16639d7b1e8944a1a1b7634b25f7e1c6a98f5
Eunice <eunicepoh98@gmail.com>
2019-10-23 16:54:32 +0800

    ViewModel for getting event categories

--- app/src/main/java/com/example/whatspoppin/ui/editpreferences/EditPreferencesFragment.java
+++ app/src/main/java/com/example/whatspoppin/ui/editpreferences/EditPreferencesFragment.java
@@ -7,10 +7,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
 import android.widget.Switch;
+
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+
 import com.example.whatspoppin.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -28,6 +31,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+
 import java.util.ArrayList;
 
 public class EditPreferencesFragment extends Fragment {
@@ -38,106 +42,64 @@ public class EditPreferencesFragment extends Fragment {
     private boolean receiveNotification = false;
     private boolean showNearbyEvents = false;
     private Switch notificationSwitch, locationSwitch;
+
     //firebase
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private FirebaseAuth mAuth;
     private DocumentReference usersDoc;
 
+    private EditPreferencesViewModel editPreferencesViewModel;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        editPreferencesViewModel = ViewModelProviders.of(this).get(EditPreferencesViewModel.class);
         View root = inflater.inflate(R.layout.fragment_editpreferences, container, false);
 
+        EditPreferencesViewModel model = ViewModelProviders.of(this).get(EditPreferencesViewModel.class);
+
+        // get current login user
         mAuth = FirebaseAuth.getInstance();
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if (currentUser != null) {
             usersDoc = db.collection("users").document(currentUser.getUid());
         }
-        return root;
-    }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        getEventCategories();
-        preferenceCG = view.findViewById(R.id.preference_chipgroup);
-        notificationSwitch = view.findViewById(R.id.notification_switch);
-        locationSwitch = view.findViewById(R.id.location_switch);
-    }
+        preferenceCG = root.findViewById(R.id.preference_chipgroup);
+        notificationSwitch = root.findViewById(R.id.notification_switch);
+        locationSwitch = root.findViewById(R.id.location_switch);
 
-    //checks firestore for realtime updates
-    public void realtimeFireStoreData() {
-        usersDoc.addSnapshotListener(new EventListener<DocumentSnapshot>() {
-            @Override
-            public void onEvent(@Nullable DocumentSnapshot snapshot,
-                                @Nullable FirebaseFirestoreException e) {
-                if (e != null) {
-                    Log.w("Listen", "Listen failed.", e);
-                    return;
-                }
-                if (snapshot != null && snapshot.exists()) {
-                    getEventCategories();
-                    getPreferenceFirestore();
-                } else {
-                    getEventCategories();
-                    getPreferenceFirestore();
-                }
-            }
-        });
-    }
-
-    public void getEventCategories() {
-        eventCategories.clear();
-        db.collection("events").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                if (task.isSuccessful()) {
-                    eventCategories.clear();
-                    for (QueryDocumentSnapshot document : task.getResult()) {
-                        if (document != null) {
-                            String category = document.getString("category");
-                            if (!eventCategories.contains(category)) {
-                                eventCategories.add(category);
-                            }
-                        } else {
-                            Log.d("", "No such document");
-                        }
-                        Log.d("", document.getId() + " => " + document.getData());
-                    }
-                    getPreferenceFirestore();
-                } else {
-                    Log.w("", "Error getting documents.", task.getException());
-                }
-            }
-        });
-    }
-
-    public void setupView() {
-
-        // category chips
+        // Setup Category Chips
         preferenceCG.setChipSpacing(20);
-        int num = 0;
-        for (String s : eventCategories) {
-            final Chip newChip = new Chip(getContext());
-            newChip.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
-                    ViewGroup.LayoutParams.WRAP_CONTENT));
-            newChip.setChipDrawable(ChipDrawable.createFromAttributes(getContext(), null,
-                    0, R.style.Widget_MaterialComponents_Chip_Filter));
-            newChip.setText(s);
-            newChip.setId(num);
-            if (categories_Selected.contains(s)) newChip.setChecked(true);
-            newChip.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        model.getEventCategories().observe(this, new Observer<ArrayList<String>>() {
                 @Override
-                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                    if (!categories_Selected.contains(newChip.getText().toString())) {
-                        categories_Selected.add(newChip.getText().toString());
-                    } else {
-                        categories_Selected.remove(newChip.getText().toString());
-                    }
-                    updatePreferenceFirestore();
+                public void onChanged(ArrayList<String> category) {
+                // category chips
+                int num = 0;
+                for (String s : category) {
+                    final Chip newChip = new Chip(getContext());
+                    newChip.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                            ViewGroup.LayoutParams.WRAP_CONTENT));
+                    newChip.setChipDrawable(ChipDrawable.createFromAttributes(getContext(), null,
+                            0, R.style.Widget_MaterialComponents_Chip_Filter));
+                    newChip.setText(s);
+                    newChip.setId(num);
+                    if (categories_Selected.contains(s)) newChip.setChecked(true);
+                    newChip.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                        @Override
+                        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                            if (!categories_Selected.contains(newChip.getText().toString())) {
+                                categories_Selected.add(newChip.getText().toString());
+                            } else {
+                                categories_Selected.remove(newChip.getText().toString());
+                            }
+                            updatePreferenceFirestore();
+                        }
+                    });
+                    preferenceCG.addView(newChip);
+                    num++;
                 }
-            });
-            preferenceCG.addView(newChip);
-            num++;
-        }
+            }
+        });
 
         // notification toggle
         if (receiveNotification) notificationSwitch.setChecked(true);
@@ -172,7 +134,76 @@ public class EditPreferencesFragment extends Fragment {
                 updatePreferenceFirestore();
             }
         });
+
+        return root;
     }
+
+//    public View onCreateView(@NonNull LayoutInflater inflater,
+//                             ViewGroup container, Bundle savedInstanceState) {
+//        View root = inflater.inflate(R.layout.fragment_editpreferences, container, false);
+//
+//        mAuth = FirebaseAuth.getInstance();
+//        FirebaseUser currentUser = mAuth.getCurrentUser();
+//        if (currentUser != null) {
+//            usersDoc = db.collection("users").document(currentUser.getUid());
+//        }
+//        return root;
+//    }
+
+//    @Override
+//    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+//        getEventCategories();
+//        preferenceCG = view.findViewById(R.id.preference_chipgroup);
+//        notificationSwitch = view.findViewById(R.id.notification_switch);
+//        locationSwitch = view.findViewById(R.id.location_switch);
+//    }
+
+    //checks firestore for realtime updates
+//    public void realtimeFireStoreData() {
+//        usersDoc.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+//            @Override
+//            public void onEvent(@Nullable DocumentSnapshot snapshot,
+//                                @Nullable FirebaseFirestoreException e) {
+//                if (e != null) {
+//                    Log.w("Listen", "Listen failed.", e);
+//                    return;
+//                }
+//                if (snapshot != null && snapshot.exists()) {
+//                    getEventCategories();
+//                    getPreferenceFirestore();
+//                } else {
+//                    getEventCategories();
+//                    getPreferenceFirestore();
+//                }
+//            }
+//        });
+//    }
+
+//    public void getEventCategories() {
+//        eventCategories.clear();
+//        db.collection("events").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//            @Override
+//            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                if (task.isSuccessful()) {
+//                    eventCategories.clear();
+//                    for (QueryDocumentSnapshot document : task.getResult()) {
+//                        if (document != null) {
+//                            String category = document.getString("category");
+//                            if (!eventCategories.contains(category)) {
+//                                eventCategories.add(category);
+//                            }
+//                        } else {
+//                            Log.d("", "No such document");
+//                        }
+//                        Log.d("", document.getId() + " => " + document.getData());
+//                    }
+//                    getPreferenceFirestore();
+//                } else {
+//                    Log.w("", "Error getting documents.", task.getException());
+//                }
+//            }
+//        });
+//    }
 
     public void updatePreferenceFirestore() {
         usersDoc.update("interests", categories_Selected)
@@ -236,7 +267,6 @@ public class EditPreferencesFragment extends Fragment {
                             }
                         } else categories_Selected = null;
                         receiveNotification = document.getBoolean("receiveNotification");
-                        ;
                         showNearbyEvents = document.getBoolean("showNearbyEvents");
 
                         Log.d("getPreferences", "DocumentSnapshot data: " + document.getData());
---
+++ app/src/main/java/com/example/whatspoppin/ui/editpreferences/EditPreferencesViewModel.java
@@ -0,0 +1,111 @@
+package com.example.whatspoppin.ui.editpreferences;
+
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class EditPreferencesViewModel extends ViewModel {
+
+    //firebase
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirebaseAuth mAuth;
+    private DocumentReference usersDoc;
+
+    private MutableLiveData<String> mText;
+    private MutableLiveData<ArrayList<String>> eventCategories;
+    private MutableLiveData<String> categoriesSelected;
+    private boolean receiveNotification = false;
+    private boolean showNearbyEvents = false;
+
+    public EditPreferencesViewModel() {
+        // get current login user
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            usersDoc = db.collection("users").document(currentUser.getUid());
+        }
+    }
+
+    public LiveData<ArrayList<String>> getEventCategories() {
+        if (eventCategories == null) {
+            eventCategories = new MutableLiveData<ArrayList<String>>();
+            getFireStoreEventsData();
+        }
+        return eventCategories;
+    }
+    public LiveData<String> getSelectedCategories() {
+        if (categoriesSelected == null) {
+            categoriesSelected = new MutableLiveData<String>();
+            getFireStoreUserData();
+        }
+        return categoriesSelected;
+    }
+
+    private void getFireStoreUserData() { // get user preferences settings
+        usersDoc.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        // get user selected categories
+                        String b = String.valueOf(document.get("interests"));
+                        if (b != "null" || b != null || b != "[]") {
+                            ArrayList<String> bkm = (ArrayList<String>) document.get("interests");
+                            for (String categoryName : bkm)
+                                categoriesSelected.setValue(categoryName);
+                        } else categoriesSelected = null;
+                        receiveNotification = document.getBoolean("receiveNotification");
+                        showNearbyEvents = document.getBoolean("showNearbyEvents");
+
+                    } else {
+                        Log.d("getUserDetails", "No such document");
+                    }
+                } else {
+                    Log.d("getUserDetails", "get failed with ", task.getException());
+                }
+            }
+        });
+    }
+
+    private void getFireStoreEventsData() {
+        // get list of all events
+        final ArrayList<String> eventCategory = new ArrayList<>();
+        db.collection("events").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        if (document != null) {
+                            String category = document.getString("category");
+                            if (!eventCategory.contains(category)) {
+                                eventCategory.add(category);
+                            }
+                        } else {
+                            Log.d("", "No such document");
+                        }
+                        Log.d("", document.getId() + " => " + document.getData());
+                    }
+                    eventCategories.setValue(eventCategory);
+                } else {
+                    Log.w("EventListFirestore", "Error getting documents.", task.getException());
+                }
+            }
+        });
+    }
+}
